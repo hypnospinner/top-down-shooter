7c7d85f57e72a7c1f378d48d26aaed78e69f05d4
Ilya Katun <hypnospinner@gmail.com>
2019-02-18 21:58:39 +0300

    added camera controller

---
+++ top-down-shooter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform Target;
+    [SerializeField] private float MovementSpeed;
+    [SerializeField] private Vector3 Offset;
+
+    private void Awake()
+    {
+        transform.LookAt(Target);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            Target.position + Offset, 
+            MovementSpeed * Time.deltaTime
+            );
+        transform.LookAt(Target);
+    }
+
+}
