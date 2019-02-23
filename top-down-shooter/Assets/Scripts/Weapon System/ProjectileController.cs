2336b3708dd773afe49168a788d686d31f43377d
Ilya Katun <hypnospinner@gmail.com>
2019-02-23 19:43:42 +0300

    added simple prjectile

---
+++ top-down-shooter/Assets/Scripts/Weapon System/ProjectileController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    #region Fields 
+
+    [SerializeField] private float AttackDistance;
+    [SerializeField] private float ProjectileSpeed;
+    [SerializeField] private LayerMask AttackMask;
+
+    private float _stepMoveDistance;
+
+    #endregion
+
+    #region Behaviour
+
+    private void Awake()
+    {
+        _stepMoveDistance = ProjectileSpeed * Time.fixedDeltaTime;
+        Destroy(gameObject, AttackDistance / ProjectileSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(
+            transform.position, 
+            transform.forward, 
+            out hit, 
+            _stepMoveDistance, 
+            AttackMask, 
+            QueryTriggerInteraction.Ignore))
+        {
+            Debug.Log("Hit " + hit.transform.name);
+            Destroy(gameObject);
+        }
+
+        transform.position += transform.forward * _stepMoveDistance;
+    }
+
+    #endregion
+
+}
