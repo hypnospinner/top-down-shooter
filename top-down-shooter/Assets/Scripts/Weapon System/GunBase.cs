6297ac09ee5a3900e91e2b3c7cc0e79ad8ff3134
Ilya Katun <hypnospinner@gmail.com>
2019-02-23 18:53:54 +0300

    added experimental gun base

---
+++ top-down-shooter/Assets/Scripts/Weapon System/GunBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class GunBase : MonoBehaviour
+{
+    #region Fields 
+
+    // delegates
+    protected delegate bool CheckForButtonState(ButtonState buttonState);
+
+    // private variables
+    protected PlayerInputController _inputController;
+    protected bool _isReady;
+
+    #endregion
+
+    #region Behaviour
+
+    protected void Awake()
+    {
+        _isReady = true;
+
+        _inputController = transform.root.GetComponent<PlayerInputController>();
+        if (_inputController == null)
+            Debug.LogError("PLayer Input Controller is not set!!!");
+    }
+
+    protected abstract void RunReload(CheckForButtonState ButtonStateChecker);
+
+    protected abstract void RunAttack(CheckForButtonState ButtonStateChecker);
+
+    protected abstract IEnumerator Reload();
+
+    protected abstract IEnumerator Attack();
+
+    #endregion
+}
+
+public enum AmmoType
+{
+    PistolAmmo
+}
