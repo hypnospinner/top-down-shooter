1b56f834cf11ba1acb7964b6224f5722c5ce935a
Ilya Katun <hypnospinner@gmail.com>
2019-02-23 22:42:00 +0300

    added asault rifle weapon

---
+++ top-down-shooter/Assets/Scripts/Weapon System/AssaultRifleController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+
+class AssaultRifleController : GunBase
+{
+    #region Fields 
+
+    [SerializeField] private float ReloadingTime;
+    [SerializeField] private float FireRate;
+    [SerializeField] private int ClipSize;
+    [SerializeField] private int MaxAmmo;
+
+    private int _clip;
+    private int _ammo;
+
+    #endregion
+
+    #region Behaviour
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _clip = ClipSize;
+        _ammo = MaxAmmo;
+
+        _reloadChecker = () => _inputController.RealoadingButton == ButtonState.Down;
+        _attackChecker = () => _inputController.LeftMouseButton == ButtonState.Down || 
+                               _inputController.LeftMouseButton == ButtonState.Hold;
+    }
+
+    protected override void RunAttack()
+    {
+        if (_isReady && _clip > 0)
+            StartCoroutine(Attack());
+    }
+
+    protected override void RunReload()
+    {
+        if (_clip < ClipSize && _ammo > 0)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    protected override IEnumerator Attack()
+    {
+        _isReady = false;
+        _clip--;
+
+        Instantiate(Projectile, Muzzle.position, Muzzle.rotation);
+
+        yield return new WaitForSeconds(FireRate);
+
+        _isReady = true;
+    }
+
+    protected override IEnumerator Reload()
+    {
+        _isReady = false;
+
+        yield return new WaitForSeconds(ReloadingTime);
+
+        if (_ammo > (ClipSize - _clip))
+        {
+            _ammo -= ClipSize - _clip;
+            _clip = ClipSize;
+        }
+        else
+        {
+            _clip += _ammo;
+            _ammo = 0;
+        }
+
+        _isReady = true;
+    }
+
+    #endregion
+}
+
