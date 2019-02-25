709891e8259d2219b82a317d88e2bbd4e3328ce1
Ilya Katun <hypnospinner@gmail.com>
2019-02-25 22:37:00 +0300

    enhanced interactive object for feedback on interaction event

--- top-down-shooter/Assets/Scripts/Interaction System/InteractiveObject.cs
+++ top-down-shooter/Assets/Scripts/Interaction System/InteractiveObject.cs
@@ -2,6 +2,10 @@
 
 public class InteractiveObject : MonoBehaviour
 {
+    protected PlayerInteractor _interactor;
+
+    public PlayerInteractor Interactor { get => _interactor; set => _interactor = value; }
+
     public virtual void Interact(GameObject interactor)
     {
         Debug.Log("Base Interaction call");
