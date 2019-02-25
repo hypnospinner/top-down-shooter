002a0e1d3d9631a8a82a1c04324393549dc78f0e
Ilya Katun <hypnospinner@gmail.com>
2019-02-25 22:37:14 +0300

    added interactive weapon script

---
+++ top-down-shooter/Assets/Scripts/Interaction System/InteractiveWeapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InteractiveWeapon : InteractiveObject
+{
+    [SerializeField] private GameObject StoredWeapon;
+
+    public override void Interact(GameObject interactor)
+    {
+        WeaponController weaponController = interactor.GetComponentInChildren<WeaponController>();
+        if (weaponController.PickNewWeapon(StoredWeapon, this))
+        {
+            _interactor.InteractedWithDestruction(this);
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetWeapon(GameObject newWeapon)
+    {
+        StoredWeapon = newWeapon;
+        StoredWeapon.transform.position = transform.position;
+        StoredWeapon.SetActive(false);
+    }
+}
