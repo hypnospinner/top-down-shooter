1b9c2e4bdff1b830ba0de9308283c57bc82928d9
Ilya Katun <hypnospinner@gmail.com>
2019-02-28 15:44:02 +0300

    added damage receiver

---
+++ top-down-shooter/Assets/Scripts/Player System/DamageSender.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+class DamageSender : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private DamageData DamageDataPrefab;
+
+    private DamageData _damageData;
+
+    #endregion
+
+    #region Behaviour
+
+    private void Awake()
+    {
+        _damageData = ScriptableObject.CreateInstance<DamageData>();
+        _damageData.SetDamageData(DamageDataPrefab);
+    }
+
+    public void SendDamageTo(GameObject damaged)
+    {
+        IDamagable damagable = damaged.GetComponentInChildren<IDamagable>(true);
+        if (damagable != null)
+            damagable.ReceiveDamage(_damageData);
+    }
+
+    #endregion
+}
+
