d9eb1320138dd312de7abfbc58332b4e1f6f3c3c
Ilya Katun <hypnospinner@gmail.com>
2019-02-28 10:20:02 +0300

    basic player controlelr setup

---
+++ top-down-shooter/Assets/Scripts/Player System/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Fields 
+
+    [SerializeField] private float _health;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _rotationSpeed;
+
+    private PlayerInputController _inputController;
+
+    public float Health { get => _health; }
+    public float MovementSpeed { get => _movementSpeed; }
+    public float RotationSpeed { get => _rotationSpeed; }
+
+    #endregion
+
+    #region Behaviour 
+
+    private void Awake()
+    {
+        _inputController = GetComponent<PlayerInputController>();
+        if (_inputController == null)
+            Debug.LogError("Player Input Controller is not set!!!");
+    }
+
+    #endregion
+}
