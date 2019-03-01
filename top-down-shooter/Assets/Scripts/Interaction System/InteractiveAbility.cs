29ffd55507e8aba1b4c420a53804272b8e9cdf38
Ilya Katun <hypnospinner@gmail.com>
2019-03-01 23:55:30 +0300

    added interactive ability

---
+++ top-down-shooter/Assets/Scripts/Interaction System/InteractiveAbility.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class InteractiveAbility : Interactive
+{
+    #region Fields 
+
+    [SerializeField] private GameObject abilityPrefab;
+
+    #endregion
+    
+    #region Behaviour 
+
+    public override void Interact(GameObject interactor)
+    {
+        AbilityController abilityController = interactor.GetComponent<AbilityController>();
+
+        if(abilityController != null)
+        {
+            abilityPrefab = abilityController.PickUpAbility(abilityPrefab);
+            if (abilityPrefab == null)
+                DestroyInteractive();
+        }
+    }
+    
+    #endregion
+}
