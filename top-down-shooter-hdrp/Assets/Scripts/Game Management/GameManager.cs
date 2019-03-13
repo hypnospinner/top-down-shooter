ec0b274686c2cba209823486d678f32caa8b10ae
Ilya Katun <hypnospinner@gmail.com>
2019-03-13 23:14:20 +0300

    actual game manager addition

---
+++ top-down-shooter-hdrp/Assets/Scripts/Game Management/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace top_down_shooter
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private GameObject PlayerPrefab;               // instantiated in the bginning of the game
+        [SerializeField] private Transform PlayerSpawn;                 // place where player should be instantiated
+
+        private PlayerController _playerController;                     // for storing data about everything that should happen
+
+        #endregion
+
+        #region Behaviour
+
+        private void Awake()
+        {
+            if (PlayerPrefab != null && PlayerSpawn != null)
+            {
+                GameObject player = Instantiate(PlayerPrefab, PlayerSpawn.position, Quaternion.identity) as GameObject;
+
+                _playerController = player.GetComponent<PlayerController>();
+
+                _playerController.OnPlayerDead +=
+                    () => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            else Debug.Log("Game Manager disable to build scene");
+        }
+
+        #endregion
+    }
+}
